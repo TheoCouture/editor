b0e2533a7bd144347dadb3f5c3bde2623feee8fd
TheoCouture <theo.couture@gmail.com>
2017-11-16 14:12:03 +0100

    Fourth Task OK

--- src/org/ulco/ID.java
+++ src/org/ulco/ID.java
@@ -1,5 +1,23 @@
 package org.ulco;
 
 public class ID {
-    static public int ID = 0;
+    private int ID = 0;
+
+    private static ID INSTANCE = null;
+
+    public static ID getInstance (){
+        if (INSTANCE == null){
+            INSTANCE = new ID();
+        }
+
+        return INSTANCE;
+    }
+
+    public int GetNumber(){
+        return ++ID;
+    }
+
+    public int GetLastNumber(){
+        return ID;
+    }
 }
