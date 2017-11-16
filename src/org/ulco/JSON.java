a5c9f4c12dec7cff8d171d746a9e56c98695dd1d
TheoCouture <theo.couture@gmail.com>
2017-11-16 13:49:34 +0100

    Second Task OK

--- src/org/ulco/JSON.java
+++ src/org/ulco/JSON.java
@@ -1,18 +1,48 @@
 package org.ulco;
 
+import java.lang.reflect.Constructor;
+
 public class JSON {
     static public GraphicsObject parse(String json) {
         GraphicsObject o = null;
         String str = json.replaceAll("\\s+", "");
         String type = str.substring(str.indexOf("type") + 5, str.indexOf(","));
+        type = type.substring(0, 1).toUpperCase() + type.substring(1);
 
-        if (type.compareTo("square") == 0) {
-            o = new Square(str);
-        } else if (type.compareTo("rectangle") == 0) {
-            o = new Rectangle(str);
-        } else if (type.compareTo("circle") == 0) {
-            o = new Circle(str);
+        try
+        {
+
+            Class<?> classe = Class.forName ("org.ulco." + type);
+            Constructor<?> constructeur = classe.getConstructor(String.class);
+            o = (GraphicsObject) constructeur.newInstance (str);
         }
+        catch (ClassNotFoundException e)
+        {
+            // La classe n'existe pas
+        }
+        catch (NoSuchMethodException e)
+        {
+            // La classe n'a pas le constructeur recherché
+        }
+        catch (InstantiationException e)
+        {
+            // La classe est abstract ou est une interface
+        }
+        catch (IllegalAccessException e)
+        {
+            // La classe n'est pas accessible
+        }
+        catch (java.lang.reflect.InvocationTargetException e)
+        {
+            // Exception déclenchée si le constructeur invoqué
+            // a lui-même déclenché une exception
+        }
+        catch (IllegalArgumentException e)
+        {
+            // Mauvais type de paramètre
+            // (Pas obligatoire d'intercepter IllegalArgumentException)
+        }
+
         return o;
     }
 
