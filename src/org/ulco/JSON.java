9493a0b300c79e13a6d70235892f3e2d6eb70012
TheoCouture <theo.couture@gmail.com>
2017-11-26 17:42:11 +0100

    Ninth Test Non Complet reste JSON vers Objet

--- src/org/ulco/JSON.java
+++ src/org/ulco/JSON.java
@@ -6,6 +6,58 @@ import java.lang.reflect.Field;
 import java.util.Vector;
 
 public class JSON {
+
+    /*static public GraphicsObject parseGraphicObject(String json){
+
+        GraphicsObject o = null;
+        String str = json.replaceAll("\\s+", "");
+        String type = str.substring(str.indexOf("type") + 5, str.indexOf(","));
+        type = type.substring(0, 1).toUpperCase() + type.substring(1);
+
+        try
+        {
+
+            Class<?> classe = Class.forName ("org.ulco." + type);
+            Constructor<?> constructeur = classe.getConstructor(String.class);
+
+            constructeur.
+
+            if (classe.getClass(). instanceof Shape)
+            {
+
+            }
+            o = (GraphicsObject) constructeur.newInstance (str);
+        }
+        catch (ClassNotFoundException e)
+        {
+            // La classe n'existe pas
+        }
+        catch (NoSuchMethodException e)
+        {
+            // La classe n'a pas le constructeur recherché
+        }
+        catch (InstantiationException e)
+        {
+            // La classe est abstract ou est une interface
+        }
+        catch (IllegalAccessException e)
+        {
+            // La classe n'est pas accessible
+        }
+        catch (java.lang.reflect.InvocationTargetException e)
+        {
+            // Exception déclenchée si le constructeur invoqué
+            // a lui-même déclenché une exception
+        }
+        catch (IllegalArgumentException e)
+        {
+            // Mauvais type de paramètre
+            // (Pas obligatoire d'intercepter IllegalArgumentException)
+        }
+
+        return o;
+
+    }*/
     static public GraphicsObject parse(String json) {
         GraphicsObject o = null;
         String str = json.replaceAll("\\s+", "");
